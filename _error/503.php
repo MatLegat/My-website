1b65e05c93bb5d428cc709b23708391b2752a8fc
Matteus Legat <matteuslegat@gmail.com>
2016-05-29 23:21:14 -0400

    fix 503 direct access

--- _error/503.php
+++ _error/503.php
@@ -4,4 +4,6 @@ if (substr($uri, 0, 16) === '/service/grades/') {
 	exec('sudo /var/www/html/restart_play.sh > /dev/null 2>/dev/null &');
 	sleep(10);
 	header("Refresh:0");
+} else {
+	header("location:$uri.");
 }
