cb3b2762198fee14cf2b583fff2a08584d277276
Matteus Legat <matteuslegat@gmail.com>
2016-06-01 22:01:05 -0400

    Updates grades submodule

--- _error/503.php
+++ _error/503.php
@@ -2,7 +2,7 @@
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 0, 16) === '/service/grades/') {
 	exec('sudo /var/www/html/restart_play.sh > /dev/null 2>/dev/null &');
-	sleep(10);
+	sleep(13);
 	header("Refresh:0");
 } else {
 	header("location:$uri.");
