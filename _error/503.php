87d77e5434843303d66a92a697e94b78bcd23976
Matteus Legat <matteuslegat@gmail.com>
2016-06-02 18:51:49 -0400

    Improves 503 correction

--- _error/503.php
+++ _error/503.php
@@ -2,7 +2,10 @@
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 0, 16) === '/service/grades/') {
 	exec('sudo /var/www/html/restart_play.sh > /dev/null 2>/dev/null &');
-	sleep(13);
+	while(!(bool)fsockopen('127.0.0.1', 9000, $errno, $errstr, 1)) {
+		sleep(1);
+	}
+	sleep(2);
 	header("Refresh:0");
 } else {
 	header("location:$uri.");
