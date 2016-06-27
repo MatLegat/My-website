014b1e03108092f80fce712e2bebaeed89d391c5
Matteus Legat <matteuslegat@gmail.com>
2016-06-27 16:48:40 -0400

    Import todos list as a submodule

--- _error/503.php
+++ _error/503.php
@@ -1,12 +1,19 @@
 <?php 
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 0, 16) === '/service/grades/') {
-	exec('sudo /var/www/html/restart_play.sh > /dev/null 2>/dev/null &');
+	exec('sudo /var/www/html/restart_play.sh > /dev/null 2> /dev/null &');
 	while(!(bool)fsockopen('127.0.0.1', 9000, $errno, $errstr, 1)) {
 		sleep(1);
 	}
 	sleep(2);
 	header("Refresh:0");
+} else if (substr($uri, 0, 16) === '/service/todo/') {
+	exec('sudo /var/www/html/restart_meteor.sh > /dev/null 2> /dev/null &');
+	while(!(bool)fsockopen('127.0.0.1', 3000, $errno, $errstr, 2)) {
+		sleep(2);
+	}
+	sleep(20);
+	header("Refresh:0");
 } else {
-	header("location:$uri.");
+	echo file_get_contents('503.html');
 }
