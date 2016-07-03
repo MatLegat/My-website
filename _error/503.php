6dc9036ad4d561e115ece71e420613b2bc222630
Matteus Legat <matteuslegat@gmail.com>
2016-07-03 19:41:35 -0400

    Update scripts path

--- _error/503.php
+++ _error/503.php
@@ -1,14 +1,14 @@
 <?php 
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 0, 16) === '/service/grades/') {
-	exec('sudo /var/www/html/restart_play.sh > /dev/null 2> /dev/null &');
+	exec('sudo /srv/legat.ml/scripts/restart_play.sh > /dev/null 2> /dev/null &');
 	while(!(bool)fsockopen('127.0.0.1', 9000, $errno, $errstr, 1)) {
 		sleep(1);
 	}
 	sleep(2);
 	header("Refresh:0");
 } else if (substr($uri, 0, 16) === '/service/todo/') {
-	exec('sudo /var/www/html/restart_meteor.sh > /dev/null 2> /dev/null &');
+	exec('sudo /srv/legat.ml/scripts/restart_meteor.sh > /dev/null 2> /dev/null &');
 	while(!(bool)fsockopen('127.0.0.1', 3000, $errno, $errstr, 2)) {
 		sleep(2);
 	}
